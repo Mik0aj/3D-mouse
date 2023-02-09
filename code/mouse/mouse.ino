68b09710ed0f2e38d178b976685d9e74d75e9c3b
Mikołaj Aleksander Mosoń <mikoajmoson@gmail.com>
2023-02-09 15:48:45 +0100

    joystick thread now uses array of pointers to set XYZ rXrYrZ, joystick thread also updates rgb lights

--- code/mouse/mouse.ino
+++ code/mouse/mouse.ino
@@ -51,7 +51,6 @@ enum TIMING {
   MPU_THREAD_TIMER = 2,
   MODE_BUTTON_INTERRUPT_TIMER = 100,
 };
-
 enum class Mode {
   XYZ,
   rXrYrZ,
@@ -72,11 +71,122 @@ typedef std::deque<float> axis;
 std::array<axis, 3> allAxis;
 std::array<float, 3> filteredValues;  //Mpu for one iteration sends max values
 std::array<float, 3> offsets;         //Mpu for one iteration sends max values
-volatile Mode deviceMode{ Mode::XYZ };
+volatile Mode deviceMode{ Mode::rXrYrZ };
 volatile Mode broadcastMode{ Mode::NOT_BROADCASTING };
 std::array<void (Joystick_::*)(long int), 3> joystickXYZsetters;
 std::array<void (Joystick_::*)(long int), 3> joystickRXRYRZsetters;
 std::array<int, 3> pinsRGB;
+
+void setup() {
+  Wire.begin();
+  Wire.setClock(I2C_CLOCK);  // 400kHz I2C clock. Comment this line if having compilation difficulties
+  Serial.begin(BAUD);
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  devStatus = mpu.dmpInitialize();
+  Serial.println(devStatus);
+  if (devStatus != 0) {
+    deviceFailed(devStatus);
+  }
+  mpu.setXGyroOffset(GYRO_X);
+  mpu.setYGyroOffset(GYRO_Y);
+  mpu.setZGyroOffset(GYRO_Z);
+  mpu.setXAccelOffset(ACCEL_X);
+  mpu.setYAccelOffset(ACCEL_Y);
+  mpu.setZAccelOffset(ACCEL_Z);
+  Serial.println(F("Enabling DMP..."));
+  mpu.setDMPEnabled(true);
+  packetSize = mpu.dmpGetFIFOPacketSize();
+  // Braced initialiser does not work for deque
+  for (auto &a : allAxis) {
+    a.push_back(0);
+    a.push_back(0);
+    a.push_back(0);
+  }
+
+  pinMode(MODE_BUTTON, INPUT);                                               //todo lookup input pullup
+  attachInterrupt(digitalPinToInterrupt(MODE_BUTTON), modeChange, FALLING);  // trigger when button pressed, but not when released.
+  pinMode(BROADCAST_BUTTON, INPUT_PULLUP);
+
+  joystick.setXAxisRange(MIN_X, MAX_X);
+  joystick.setYAxisRange(MIN_Y, MAX_Y);
+  joystick.setZAxisRange(MIN_Z, MAX_Z);
+  joystick.setRxAxisRange(MIN_X, MAX_X);
+  joystick.setRyAxisRange(MIN_Y, MAX_Y);
+  joystick.setRzAxisRange(MIN_Z, MAX_Z);
+
+  // axis numbers set to my preference
+  joystickXYZsetters.at(2) = { &joystick.setXAxis };
+  joystickXYZsetters.at(1) = { &joystick.setYAxis };
+  joystickXYZsetters.at(0) = { &joystick.setZAxis };
+  joystickRXRYRZsetters.at(0) = { &joystick.setRxAxis };
+  joystickRXRYRZsetters.at(1) = { &joystick.setRyAxis };
+  joystickRXRYRZsetters.at(2) = { &joystick.setRzAxis };
+
+  pinsRGB.at(0) = { RED_PIN };
+  pinsRGB.at(1) = { GREEN_PIN };
+  pinsRGB.at(2) = { BLUE_PIN };
+
+  PT_INIT(&printThread);
+  PT_INIT(&joystickThread);
+  PT_INIT(&mpuThread);
+}
+
+// todo introduce constant
+void modeChange() {
+  static unsigned long last_interrupt_time = 0;
+  if (millis() - last_interrupt_time > MODE_BUTTON_INTERRUPT_TIMER) {
+    switch (deviceMode) {
+      case Mode::XYZ:
+        deviceMode = Mode::rXrYrZ;
+        break;
+      case Mode::rXrYrZ:
+        deviceMode = Mode::XYZ;
+        break;
+      default:
+        Serial.print("Wrong mode chceck: ");
+        Serial.print(__LINE__);
+        deviceFailed(10);
+        break;
+    }
+    // need to reset axis
+    joystick.setXAxis(0);
+    joystick.setYAxis(0);
+    joystick.setZAxis(0);
+    joystick.setRxAxis(0);
+    joystick.setRyAxis(0);
+    joystick.setRzAxis(0);
+  }
+  last_interrupt_time = millis();
+}
+
+void deviceFailed(int code) {
+  // (if it's going to break, usually the code will be 1)
+  // 1 = initial memory load failed
+  // 2 = DMP configuration updates failed
+  // 10 = Wrong mode detected
+  while (1) {
+    Serial.print(F("Device failed (code "));
+    Serial.print(code);
+    Serial.println(F(")"));
+    delay(1000);
+    analogWrite(RED_PIN, 1024);
+    delay(1000);
+    analogWrite(RED_PIN, 0);
+  }
+}
+
+float median(axis &currentAxis) {
+  float min{ currentAxis.at(0) };
+  float max{ currentAxis.at(0) };
+  float sum{ 0 };
+  for (auto &val : currentAxis) {
+    max = (val > max) ? val : max;
+    min = (val < min) ? val : min;
+    sum += val;
+  }
+  return sum - max - min;
+}
 
 static void protothreadPrintSerial(struct pt *pt) {
   static unsigned long lastTimePrint = 0;
@@ -124,7 +234,7 @@ static void protothreadPrintSerial(struct pt *pt) {
 }
 
 void rgbUpdate(int pinIndex, int value) {
-  analogWrite(pinsRGB.at(pinIndex),map(value,-2000,2000,0,1024));
+  analogWrite(pinsRGB.at(pinIndex), map(value, -2000, 2000, 0, 255));
 }
 
 static void protothreadJoystickBroadcast(struct pt *pt) {
@@ -157,6 +267,9 @@ static void protothreadJoystickBroadcast(struct pt *pt) {
         }
         break;
       case Mode::NOT_BROADCASTING:
+        for (int pinIndex{ 0 }; pinIndex < pinsRGB.size(); pinIndex++) {
+          rgbUpdate(pinIndex, 0);
+        }
         break;
       default:
         Serial.print("Wrong mode chceck: ");
@@ -190,116 +303,6 @@ static void protothreadMpuUpdate(struct pt *pt) {
     }
   }
   PT_END(pt);
-}
-
-void deviceFailed(int code) {
-  // (if it's going to break, usually the code will be 1)
-  // 1 = initial memory load failed
-  // 2 = DMP configuration updates failed
-  // 10 = Wrong mode detected
-  while (1) {
-    Serial.print(F("Device failed (code "));
-    Serial.print(code);
-    Serial.println(F(")"));
-    delay(1000);
-    analogWrite(RED_PIN, 1024);
-    delay(1000);
-    analogWrite(RED_PIN, 0);
-  }
-}
-
-float median(axis &currentAxis) {
-  float min{ currentAxis.at(0) };
-  float max{ currentAxis.at(0) };
-  float sum{ 0 };
-  for (auto &val : currentAxis) {
-    max = (val > max) ? val : max;
-    min = (val < min) ? val : min;
-    sum += val;
-  }
-  return sum - max - min;
-}
-
-void setup() {
-  Wire.begin();
-  Wire.setClock(I2C_CLOCK);  // 400kHz I2C clock. Comment this line if having compilation difficulties
-  Serial.begin(BAUD);
-  delay(1000);
-  Serial.println(F("Initializing I2C devices..."));
-  mpu.initialize();
-  devStatus = mpu.dmpInitialize();
-  Serial.println(devStatus);
-  if (devStatus != 0) {
-    deviceFailed(devStatus);
-  }
-  mpu.setXGyroOffset(GYRO_X);
-  mpu.setYGyroOffset(GYRO_Y);
-  mpu.setZGyroOffset(GYRO_Z);
-  mpu.setXAccelOffset(ACCEL_X);
-  mpu.setYAccelOffset(ACCEL_Y);
-  mpu.setZAccelOffset(ACCEL_Z);
-  Serial.println(F("Enabling DMP..."));
-  mpu.setDMPEnabled(true);
-  packetSize = mpu.dmpGetFIFOPacketSize();
-  // Braced initialiser does not work for deque
-  for (auto &a : allAxis) {
-    a.push_back(0);
-    a.push_back(0);
-    a.push_back(0);
-  }
-  pinMode(MODE_BUTTON, INPUT);                                               //todo lookup input pullup
-  attachInterrupt(digitalPinToInterrupt(MODE_BUTTON), modeChange, FALLING);  // trigger when button pressed, but not when released.
-  pinMode(BROADCAST_BUTTON, INPUT_PULLUP);
-
-  joystick.setXAxisRange(MIN_X, MAX_X);
-  joystick.setYAxisRange(MIN_Y, MAX_Y);
-  joystick.setZAxisRange(MIN_Z, MAX_Z);
-  joystick.setRxAxisRange(MIN_X, MAX_X);
-  joystick.setRyAxisRange(MIN_Y, MAX_Y);
-  joystick.setRzAxisRange(MIN_Z, MAX_Z);
-
-  pinsRGB.at(0)={ RED_PIN };
-  pinsRGB.at(1)={ BLUE_PIN };
-  pinsRGB.at(2)={ BLUE_PIN };
-
-  joystickXYZsetters.at(0) = { &joystick.setXAxis };
-  joystickXYZsetters.at(1) = { &joystick.setYAxis };
-  joystickXYZsetters.at(2) = { &joystick.setZAxis };
-  joystickRXRYRZsetters.at(0) = { &joystick.setRxAxis };
-  joystickRXRYRZsetters.at(1) = { &joystick.setRyAxis };
-  joystickRXRYRZsetters.at(2) = { &joystick.setRzAxis };
-
-  PT_INIT(&printThread);
-  PT_INIT(&joystickThread);
-  PT_INIT(&mpuThread);
-}
-
-// todo introduce constant
-void modeChange() {
-  static unsigned long last_interrupt_time = 0;
-  if (millis() - last_interrupt_time > MODE_BUTTON_INTERRUPT_TIMER) {
-    switch (deviceMode) {
-      case Mode::XYZ:
-        deviceMode = Mode::rXrYrZ;
-        break;
-      case Mode::rXrYrZ:
-        deviceMode = Mode::XYZ;
-        break;
-      default:
-        Serial.print("Wrong mode chceck: ");
-        Serial.print(__LINE__);
-        deviceFailed(10);
-        break;
-    }
-    // need to reset axis
-    joystick.setXAxis(0);
-    joystick.setYAxis(0);
-    joystick.setZAxis(0);
-    joystick.setRxAxis(0);
-    joystick.setRyAxis(0);
-    joystick.setRzAxis(0);
-  }
-  last_interrupt_time = millis();
 }
 
 void loop() {
